f6526ae18ea75473bc089b9d5c7c07db1172d1c5
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-02-19 22:05:21 +0000

    Base components with JSX

--- 02.Components-Basic Idea/Movie-List/src/App.jsx
+++ 02.Components-Basic Idea/Movie-List/src/App.jsx
@@ -1,10 +1,14 @@
-function App() {
+import Body from "./components/Body";
+import Header from "./components/header";
 
+function App() {
   return (
     <>
-      
+      <Header />
+      <Body />
+      <footer>Lorem, ipsum.</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 02.Components-Basic Idea/Movie-List/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+    <header>
+      <h1>Hello world!</h1>
+      <h2>Lorem, ipsum.</h2>
+    </header>)
+}
