178508b88972396a7dcae5ffb3f86a32172cee1a
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-02-19 09:47:43 +0000

    add Header component

--- 01.Introduction to React and JSX/React-Vite/src/App.jsx
+++ 01.Introduction to React and JSX/React-Vite/src/App.jsx
@@ -1,14 +1,12 @@
-function App() {
+import Header from "./components/Header";
 
+function App() {
   return (
     <>
-<header>
-  <h1>Hello frm React + Vite!</h1>
-  <h1>Best framework ever!</h1>
-  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, alias. Quibusdam dolores, maiores minus vitae illo temporibus quis quaerat optio vero repellat eligendi explicabo ut voluptates corrupti nobis ea dolor.</p>
-</header>
+    <Header />
+      <footer>Framework footer!</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 01.Introduction to React and JSX/React-Vite/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <h1>Hello frm React + Vite!</h1>
+      <h1>Best framework ever!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, alias.
+        Quibusdam dolores, maiores minus vitae illo temporibus quis quaerat
+        optio vero repellat eligendi explicabo ut voluptates corrupti nobis ea
+        dolor.
+      </p>
+    </header>
+  );
+}
+
+export default Header;
