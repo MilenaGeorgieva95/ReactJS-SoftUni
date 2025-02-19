87b8b21b51637111b460639e8351835355c5221b
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-02-19 09:42:26 +0000

    create basic page

--- 01.Introduction to React and JSX/React-Vite/src/App.jsx
+++ 01.Introduction to React and JSX/React-Vite/src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<header>
+  <h1>Hello frm React + Vite!</h1>
+  <h1>Best framework ever!</h1>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, alias. Quibusdam dolores, maiores minus vitae illo temporibus quis quaerat optio vero repellat eligendi explicabo ut voluptates corrupti nobis ea dolor.</p>
+</header>
     </>
   )
 }
