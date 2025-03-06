15a5c9f59251027858b2c9dfea08487a66504d69
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-03-06 10:27:02 +0000

    add tailwind styling to UncontrolledForm

--- 05.Forms/src/components/UncontrolledForm.jsx
+++ 05.Forms/src/components/UncontrolledForm.jsx
@@ -25,19 +25,53 @@ export default function UncontrolledForm() {
   };
   return (
     <>
-      <h2>Uncontrolled Form</h2>
+      <h2 className="text-2xl font-semibold text-center mb-6">
+        Uncontrolled Form
+      </h2>
 
-      <form onSubmit={submitHandler}>
-        <div>
-          <label htmlFor="username">Username: </label>
-          <input type="text" id="username" name="username" />
+      <form
+        onSubmit={submitHandler}
+        className="bg-white p-8 rounded-lg shadow-lg w-96 mx-auto"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="username"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="mt-2 p-3 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
         </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input type="text" id="password" name="password" />
+
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password:
+          </label>
+          <input
+            type="text"
+            id="password"
+            name="password"
+            className="mt-2 p-3 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
         </div>
+
         <div>
-          <input type="submit" value="Login" disabled={pending} />
+          <input
+            type="submit"
+            value="Login"
+            disabled={pending}
+            className={`w-full py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+              pending ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+          />
         </div>
       </form>
     </>
